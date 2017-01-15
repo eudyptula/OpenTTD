936792a7fda47c422a58d00b5a2356eef83cb594
Thomas Pedersen <panic.mailbox@gmail.com>
2017-01-15 10:29:02 +0100

    Fixed typo in comment

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -263,7 +263,7 @@
  *  193   26802
  *  194   26881   1.5.x, 1.6.0
  *  195   27572   1.6.x
- *  196   Automated timetables and serparation
+ *  196   Automated timetables and separation
  */
 extern const uint16 SAVEGAME_VERSION = 196; ///< Current savegame version of OpenTTD.
 
