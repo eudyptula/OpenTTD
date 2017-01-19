3c5c498bc224bf2991b3e935f60b44370c72f8b9
Thomas Pedersen <panic.mailbox@gmail.com>
2017-01-19 09:31:33 +0100

    Changed indentations to tabs

--- src/departures.cpp
+++ src/departures.cpp
@@ -111,7 +111,7 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 			continue;
 		}
 
-        /* MAX_COMPANIES is probably the wrong thing to put here, but it works. GenerateVehicleSortList doesn't check the company when the type of list is VL_STATION_LIST (r20801). */
+		/* MAX_COMPANIES is probably the wrong thing to put here, but it works. GenerateVehicleSortList doesn't check the company when the type of list is VL_STATION_LIST (r20801). */
 		if (!GenerateVehicleSortList(&vehicles, VehicleListIdentifier(VL_STATION_LIST, (VehicleType)(VEH_TRAIN + i), MAX_COMPANIES, station))) {
 			/* Something went wrong: panic! */
 			return result;
@@ -417,45 +417,45 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 
 			if (found_terminus) {
 				/* Add the departure to the result list. */
-                bool duplicate = false;
+				bool duplicate = false;
 
-                if (_settings_client.gui.departure_merge_identical) {
-                    for (uint i = 0; i < result->Length(); ++i) {
-                        if (*d == **(result->Get(i))) {
-                            duplicate = true;
-                            break;
-                        }
-                    }
-                }
+				if (_settings_client.gui.departure_merge_identical) {
+					for (uint i = 0; i < result->Length(); ++i) {
+						if (*d == **(result->Get(i))) {
+							duplicate = true;
+							break;
+						}
+					}
+				}
 
-                if (!duplicate) {
-                    *(result->Append(1)) = d;
+				if (!duplicate) {
+					*(result->Append(1)) = d;
 
-                    if (_settings_client.gui.departure_smart_terminus && type == D_DEPARTURE) {
-                        for (uint i = 0; i < result->Length()-1; ++i) {
-                            Departure *d_first = *(result->Get(i));
-                            uint k = d_first->calling_at.Length()-2;
-                            for (uint j = d->calling_at.Length(); j > 0; --j) {
-                                CallAt c = CallAt(*(d->calling_at.Get(j-1)));
+					if (_settings_client.gui.departure_smart_terminus && type == D_DEPARTURE) {
+						for (uint i = 0; i < result->Length()-1; ++i) {
+							Departure *d_first = *(result->Get(i));
+							uint k = d_first->calling_at.Length()-2;
+							for (uint j = d->calling_at.Length(); j > 0; --j) {
+								CallAt c = CallAt(*(d->calling_at.Get(j-1)));
 
-                                if (d_first->terminus >= c && d_first->calling_at.Length() >= 2) {
-                                    d_first->terminus = CallAt(*(d_first->calling_at.Get(k)));
+								if (d_first->terminus >= c && d_first->calling_at.Length() >= 2) {
+									d_first->terminus = CallAt(*(d_first->calling_at.Get(k)));
 
-                                    if (k == 0) break;
+									if (k == 0) break;
 
-                                    k--;
-                                }
-                            }
-                        }
-                    }
+									k--;
+								}
+							}
+						}
+					}
 
-                    /* If the vehicle is expected to be late, we want to know what time it will arrive rather than depart. */
-                    /* This is done because it looked silly to me to have a vehicle not be expected for another few days, yet it be at the same time pulling into the station. */
-                    if (d->status != D_ARRIVED &&
-                            d->lateness > 0) {
-                        d->lateness -= least_order->order->GetWaitTime();
-                    }
-                }
+					/* If the vehicle is expected to be late, we want to know what time it will arrive rather than depart. */
+					/* This is done because it looked silly to me to have a vehicle not be expected for another few days, yet it be at the same time pulling into the station. */
+					if (d->status != D_ARRIVED &&
+					    d->lateness > 0) {
+						d->lateness -= least_order->order->GetWaitTime();
+					}
+				}
 			}
 		} else {
 			/* Computing arrivals: */
@@ -471,10 +471,10 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 
 			while (candidate_origin != least_order->order) {
 				if ((candidate_origin->GetLoadType() != OLFB_NO_LOAD ||
-						_settings_client.gui.departure_show_all_stops) &&
-						(candidate_origin->GetType() == OT_GOTO_STATION ||
-						candidate_origin->GetType() == OT_IMPLICIT) &&
-						candidate_origin->GetDestination() != station) {
+				     _settings_client.gui.departure_show_all_stops) &&
+				    (candidate_origin->GetType() == OT_GOTO_STATION ||
+				     candidate_origin->GetType() == OT_IMPLICIT) &&
+				    candidate_origin->GetDestination() != station) {
 					const Order *o = (candidate_origin->next == NULL) ? least_order->v->GetFirstOrder() : candidate_origin->next;
 					bool found_collision = false;
 
@@ -486,9 +486,9 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 						}
 
 						if ((o->GetType() == OT_GOTO_STATION ||
-								o->GetType() == OT_IMPLICIT) &&
-								(o->GetDestination() == candidate_origin->GetDestination() ||
-								o->GetDestination() == station)) {
+						     o->GetType() == OT_IMPLICIT) &&
+						    (o->GetDestination() == candidate_origin->GetDestination() ||
+						     o->GetDestination() == station)) {
 							found_collision = true;
 							break;
 						}
@@ -510,8 +510,8 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 
 			while (order != least_order->order) {
 				if (order->GetType() == OT_GOTO_STATION &&
-						(order->GetLoadType() != OLFB_NO_LOAD ||
-						_settings_client.gui.departure_show_all_stops)) {
+				    (order->GetLoadType() != OLFB_NO_LOAD ||
+				     _settings_client.gui.departure_show_all_stops)) {
 					*(d->calling_at.Append(1)) = CallAt((StationID)order->GetDestination());
 				}
 
@@ -521,20 +521,20 @@ DepartureList* MakeDepartureList(StationID station, bool show_vehicle_types[5], 
 			d->terminus = CallAt((StationID)candidate_origin->GetDestination());
 
 			if (found_origin) {
-                bool duplicate = false;
+				bool duplicate = false;
 
-                if (_settings_client.gui.departure_merge_identical) {
-                    for (uint i = 0; i < result->Length(); ++i) {
-                        if (*d == **(result->Get(i))) {
-                            duplicate = true;
-                            break;
-                        }
-                    }
-                }
+				if (_settings_client.gui.departure_merge_identical) {
+					for (uint i = 0; i < result->Length(); ++i) {
+						if (*d == **(result->Get(i))) {
+							duplicate = true;
+							break;
+						}
+					}
+				}
 
-                if (!duplicate) {
-                    *(result->Append(1)) = d;
-                }
+				if (!duplicate) {
+					*(result->Append(1)) = d;
+				}
 			}
 		}
 
